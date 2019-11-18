ac43b2af996e2a9cf164913860bd458d8cca1e2e
Joshua Summers <J.Eichler-Summers@sms.ed.ac.uk>
2019-11-18 10:07:55 +0000

    authentificaation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+     def configure_permitted_parameters
+      # For additional fields in app/views/devise/registrations/new.html.erb
+      # devise_parameter_sanitizer is a function of devise
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+      # you would need to go to the nvew view, and add
+      f.input :first_name
+      f.input :last_name
+      # and add this to the controller?
+      # but don't hafe first name and last name
+      # would need to create a migration
+
+
+      # For additional in app/views/devise/registrations/edit.html.erb
+      # devise provides stuff to update account information
+      devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
